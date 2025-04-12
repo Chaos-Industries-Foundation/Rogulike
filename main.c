e2aaf0042a46f50484cc85fec01c2dc8a66559e8
Marsty-dev <maksym.rogovik@gmail.com>
2025-04-12 19:27:09 +0200

    movement done

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
 #include <curses.h>
 #include <stdio.h>
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-
 int
 main()
 {
   char main_character = '@';
-
-  WINDOW *my_win;
-  int startx, starty, width, height;
 
   initscr();
   cbreak();
@@ -20,15 +15,8 @@ main()
   int main_ch_pos_x = COLS / 2;
   int main_ch_pos_y = LINES / 2;
   
-  height = LINES - 4;
-  width = COLS - 15;
-  starty = (LINES - height) / 2;
-  startx = (COLS - width) / 2;
-
- 
   
-  for(;;){
-    my_win = create_newwin(height, width, starty, startx);  
+  for(;;){  
     mvaddch(main_ch_pos_y, main_ch_pos_x, main_character);
     move(main_ch_pos_y, main_ch_pos_x);
     int input = getch();
@@ -37,7 +25,7 @@ main()
     if(input == KEY_LEFT){
       mvaddch(main_ch_pos_y, main_ch_pos_x, ' ');
       refresh();
-     
+
       main_ch_pos_x -= 1;
       mvaddch(main_ch_pos_y, main_ch_pos_x, main_character);
       move(main_ch_pos_y, main_ch_pos_x);
@@ -71,6 +59,8 @@ main()
       move(main_ch_pos_y, main_ch_pos_x);
     }
 
+    
+
     if(input == 27)
       break;
     
@@ -79,13 +69,3 @@ main()
   return 0;
 }
 
-WINDOW *create_newwin(int height, int width, int starty, int startx) {
-  WINDOW *local_win;
-
-  local_win = newwin(height, width, starty, startx);
-  box(local_win, 0, 0);
-
-  wrefresh(local_win);
-  
-  return local_win;
-}
